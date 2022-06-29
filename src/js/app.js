fa5bae77bde2ad2c76f9fb81a205ddd423825256
Leandro Avila - L3anAv <leandroav.dev@gmail.com>
2022-06-29 00:04:42 -0300

    Últimos Cambios

--- src/js/app.js
+++ src/js/app.js
@@ -7,20 +7,23 @@ import { devolverDivRegistrarse } from "./components/divRegistrarse.js"
 
 // --> Variables Principales
 const $ROOT = document.getElementById('main'); 
-const donde = 'movies';
+const donde = '/';
 const $footer = footer()
+
 // --> Funcion Principal de la SPA
 export function App(){
 
     // --> Header con Menu De Navegacion
     $ROOT.insertAdjacentElement('beforebegin', devolverHeader())
-    
+
     // --> Contenido Del Cuerpo de la SPA
     $ROOT.appendChild(SliderParaBody(donde))
     $ROOT.insertAdjacentElement('afterbegin', contenidoParaBody(donde));
     $ROOT.insertAdjacentElement('afterend', $footer)
 
+    // --> Contenido Del Footer
     $footer.insertAdjacentElement('beforebegin', devolverDivRegistrarse())
+    
     // --> Funcionalidad Para Boton de: ir al top de SPA
     devolverBotonIrArriba()
 
